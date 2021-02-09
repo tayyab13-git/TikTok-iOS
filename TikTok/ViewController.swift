36e1ff5a576d1b24cf5a6584195d4edbcdc10e88
Tayyab <tayyab@AppsGenii-MacBook-Free-2.local>
2021-02-09 20:49:42 +0500

    project Setup

---
+++ TikTok/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TikTok
+//
+//  Created by Tayyab on 09/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
